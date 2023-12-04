206c8bdaa1066749c36762f78f7c3e45f2a6aad9
n-j-p <n-j-p@users.noreply.github.com>
2023-12-04 17:37:47 +1100

    Day 4, Part 2.

--- day4.py
+++ day4.py
@@ -2,19 +2,30 @@ fpath = './day4_input.txt'
 
 c = 0
 
+scores = {}
+copies = {}
+card = 1
 for line in open(fpath).read().split('\n'):
     if len(line) == 0:
         break
     print(line)
+    if card not in copies:
+        copies[card] = 0
     pre, nums = line.split(':')
     winners, actual = nums.split('|')
     wnum = [int(x) for x in winners.split()]
     nums = [int(x) for x in actual.split()]
     winning_numbers = set(wnum).intersection(nums)
-    if len(winning_numbers) == 0:
-        score = 0
-    else:
-        score = 2**(len(winning_numbers)-1)
-    print(f'  {len(winning_numbers)} numbers: score = {score}')
-    c += score
-print('\n',c)
+    original_score = len(winning_numbers)
+    print(f'  {original_score} matching numbers, multiplier = {copies[card]+1}')
+    for i in range(original_score):
+        try:
+            copies[card + i + 1] += copies[card] + 1
+        except KeyError:
+            copies[card + i + 1] = copies[card] + 1
+    print('  ',copies)
+
+    card += 1
+
+
+print('\n',sum(copies.values()) + card-1)
