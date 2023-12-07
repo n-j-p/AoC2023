86181bb6f58a8f46c0a7741766486ec1e38c8c41
n-j-p <n-j-p@users.noreply.github.com>
2023-12-07 22:32:43 +1100

    Day 7, Part 2.

--- day7.py
+++ day7.py
@@ -30,23 +30,71 @@ def classify(hand):
     return 6#'highc'
 
 mapping = {x:i for i,x in enumerate(['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'])}
+mapping2 = {x:i for i,x in enumerate(['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'])}
 
-def lexsort(alist):
+def lexsort(alist,mapping_):
     sorted_list = list(alist)
     n = len(alist[0][0])
     for i in range(n):
-        print([y[0][n-i-1] for y in alist])
+        #print([y[0][n-i-1] for y in alist])
         sorted_list = sorted(sorted_list,
-                             key = lambda y: mapping[y[0][n-i-1]])
-        print(sorted_list)
+                             key = lambda y: mapping_[y[0][n-i-1]])
+        #print(sorted_list)
     return sorted_list
 
+import itertools as it
+nonJ = [x for x in mapping.keys() if x != 'J']
+def replaceJ(hand):
+    nJ = hand.count('J')
+    iJ = [-1]
+    for n in range(nJ):
+        iJ.append(hand.index('J',iJ[-1]+1))
+    iJ.pop(0)
+                  
+    replacements = it.product(nonJ, repeat=nJ)
+    for repl in replacements:
+        newhand = list(hand)
+        for j,i in enumerate(iJ):
+            newhand[i] = repl[j]
+        yield ''.join(newhand)
+
+def classify_wild(hand):
+    #print(hand)
+    if 'J' not in hand[0]:
+        return classify(hand)
+    best_rank = 999
+    replaced = [(x,classify([x,None])) for x in replaceJ(hand[0])]
+    #print('  ',sorted(replaced, key=lambda x:x[1]))
+    return min([x[1] for x in replaced])
+        
 if testing:
-    ranked = sorted(lexsort([x.split() for x in example_input]),
+    ranked = sorted(lexsort([x.split() for x in example_input], mapping),
                     key = lambda y: classify(y))
 else:
-    ranked = sorted(lexsort([x.split() for x in actual_input]),
+    ranked = sorted(lexsort([x.split() for x in actual_input], mapping),
                     key = lambda y: classify(y))
+
+winnings = 0
+for x in zip(range(1,len(ranked)+1),ranked[::-1]):
+    rank = x[0]
+    bid = int(x[1][1])
+    winnings += rank * bid
+    print(x)
+print('\n',winnings)
+
+### Part 2 ###
+
+if testing:
+    #print('\n')
+    #print(lexsort([x.split() for x in example_input], mapping2))
+    #print('\n')
+    #print([(y, classify_wild(y)) for y in lexsort([x.split() for x in example_input], mapping2)])
+    #print('\n')
+    ranked = sorted(lexsort([x.split() for x in example_input], mapping2),
+                    key = lambda y: classify_wild(y))
+else:
+    ranked = sorted(lexsort([x.split() for x in actual_input], mapping2),
+                    key = lambda y: classify_wild(y))
 
 winnings = 0
 for x in zip(range(1,len(ranked)+1),ranked[::-1]):
